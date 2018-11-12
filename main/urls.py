a5a78148958b4ec1eaf074f0db4f63158679d17d
fedotsoldier <mumikrol@gmail.com>
2018-11-12 20:47:58 +0300

    [ 2 ] Добавлена база данных со страницами. Изменена структура frontend составляющей
        Добавлена база данных со страницами. URL до нужной страницы должен иметь вид "post/number", где number - поле sort этой страницы в базе данных.
        Изменена структура frontend составляющей.

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,15 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.main_page, name='main_page'),
-    path('test', views.test_page, name='test_page'),
+	path('', views.main_page),
+    path('page/0', views.main_page, name='main_page'),
+    path('page/1', views.test_page, name='test_page'),
 ]
+
+'''
+mysite - historicsite
+blog - main
+Post - Page
+'''
+# номер должен совпадать с полем sort
+# у соответствующего поста в бд
--- main/views.py
+++ main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+
+# Все нащи страницы - записи в бд Pages
+pages = Page.objects.order_by('sort')
+
 def main_page(request):
-	return render(request, 'main/main_page.html', {})
+	return render(request, 'main/main_page.html', {'pages':pages})
 
 def test_page(request):
-	return render(request, 'main/test_page.html')
+	return render(request, 'main/test_page.html', {'pages':pages})
