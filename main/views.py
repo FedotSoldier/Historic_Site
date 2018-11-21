e7be4c0541a106a1a0698857cd47e4b34a0bfa4f
fedotsoldier <mumikrol@gmail.com>
2018-11-21 22:37:09 +0300

    [ 4 ] Добавлен переход к следующей и предыдущим страницам
    Теперь нормально функционируют кнопки перехода на следующую и предыдущую страницы. Если пользователь находится на последней странице, при нажатии на кнопку следующей страницы его вернет на первую, аналогично с первой при переходе назад идет перенаправление на последнюю страницу

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+# from django.shortcuts import get_object_or_404
 from .models import Page
 
 # Create your views here.
@@ -6,6 +7,31 @@ from .models import Page
 def page_detail(request, pattern):
 	# Все нащи страницы - записи в бд Pages
 	pages = Page.objects.order_by('sort')
+
 	#page = get_object_or_404(Page, urlpattern_name=pattern)
 	#return render(request, 'main/page_detail.html', {'page':page})
-	return render(request, 'main/{}.html'.format(pattern), {'pages':pages})
+
+	''' Получаем объекты следующего и предыдущего постов '''
+	
+	# Получаем индекс элемента данного поста в queryset
+	index = list(pages).index(Page.objects.get(urlpattern_name=pattern))
+
+	# Получаем индекс следующего элемента
+	# (для кнопок следующей и предыдущей страниц)
+	index = (index + 1) % len(pages)
+
+	# Следующий пост
+	next_page = list(pages)[index].urlpattern_name
+
+	# Предыдущий пост
+	prev_page = list(pages)[index - 2].urlpattern_name
+
+	return render(
+				  request,
+				  'main/{}.html'.format(pattern),
+				  	{
+				  	'pages':pages,
+				  	'prev_page':prev_page,
+				  	'next_page':next_page
+				  	 }
+				 )
