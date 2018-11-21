de48f0d39d3fa29dcfba9dc83247e33f2a65cd2c
fedotsoldier <mumikrol@gmail.com>
2018-11-21 18:13:16 +0300

    [ 3 ] Добавлено динамическое добавление страницы на сайт, по ее созданию и определению в базе данных
        Теперь для добавления страницы на сайт достаточно создать страницу(.html файл) по образцу(создать ее как расширение django-шаблона), поместить ее в директорию"main\templates\main" и определить в базе данных, указав три поля: Название - вид отображения страницы на ссылке и тп, Имя шаблона - имя html файла страницы в вышеупомянутой директории без расширения, Поле Sort - порядок отображения страницы в списке страниц(и везде, если еще где-нибудь это понадобится)

--- main/urls.py
+++ main/urls.py
@@ -3,9 +3,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-	path('', views.main_page),
-    path('page/0', views.main_page, name='main_page'),
-    path('page/1', views.test_page, name='test_page'),
+	path('', views.page_detail, {'pattern':'main_page'}),
+	path('page/<slug:pattern>/', views.page_detail, name='page_detail'),
 ]
 
 '''
@@ -13,5 +12,3 @@ mysite - historicsite
 blog - main
 Post - Page
 '''
-# номер должен совпадать с полем sort
-# у соответствующего поста в бд
--- main/views.py
+++ main/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Page
 
 # Create your views here.
 
-# Все нащи страницы - записи в бд Pages
-pages = Page.objects.order_by('sort')
-
-def main_page(request):
-	return render(request, 'main/main_page.html', {'pages':pages})
-
-def test_page(request):
-	return render(request, 'main/test_page.html', {'pages':pages})
+def page_detail(request, pattern):
+	# Все нащи страницы - записи в бд Pages
+	pages = Page.objects.order_by('sort')
+	#page = get_object_or_404(Page, urlpattern_name=pattern)
+	#return render(request, 'main/page_detail.html', {'page':page})
+	return render(request, 'main/{}.html'.format(pattern), {'pages':pages})
