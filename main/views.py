3d016d97909bf3dcc6c7f31a94d0f334803ee9d3
fedotsoldier <mumikrol@gmail.com>
2018-12-07 21:06:02 +0300

    [ 5 ] Начала добавляться информационная составляющая сайта. Исправлена ошибка в представлении при переходе на предыдущую страницу
    На сайте появился контент. Теперь уже он напрямую связан с обороной Минска 1941 года.
    
    Исправлена ошибка перехода на предыдущую страницу, возникающая в том случае, если в базе есть только одна страница

--- main/views.py
+++ main/views.py
@@ -8,13 +8,16 @@ def page_detail(request, pattern):
 	# Все нащи страницы - записи в бд Pages
 	pages = Page.objects.order_by('sort')
 
-	#page = get_object_or_404(Page, urlpattern_name=pattern)
-	#return render(request, 'main/page_detail.html', {'page':page})
+	# page = get_object_or_404(Page, urlpattern_name=pattern)
+	# return render(request, 'main/page_detail.html', {'page':page})
 
-	''' Получаем объекты следующего и предыдущего постов '''
+	''' Получаем объекты текущего, следующего и предыдущего постов '''
+
+	# Получаем объект текущей страницы
+	curr_page = Page.objects.get(urlpattern_name=pattern)
 	
 	# Получаем индекс элемента данного поста в queryset
-	index = list(pages).index(Page.objects.get(urlpattern_name=pattern))
+	index = list(pages).index(curr_page)
 
 	# Получаем индекс следующего элемента
 	# (для кнопок следующей и предыдущей страниц)
@@ -24,13 +27,18 @@ def page_detail(request, pattern):
 	next_page = list(pages)[index].urlpattern_name
 
 	# Предыдущий пост
-	prev_page = list(pages)[index - 2].urlpattern_name
+	# Если на сайте всего одна страница:
+	if len(pages) == 1:
+		prev_page = next_page
+	else:
+		prev_page = list(pages)[index - 2].urlpattern_name
 
 	return render(
 				  request,
 				  'main/{}.html'.format(pattern),
 				  	{
 				  	'pages':pages,
+				  	'curr_page':curr_page,
 				  	'prev_page':prev_page,
 				  	'next_page':next_page
 				  	 }
